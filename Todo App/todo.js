11a21a758403f9554fb2a100ed9e3dc7ae87548a
shahinchowdory02 <shaheenchowdory80250@gmail.com>
2025-12-26 22:08:56 +0600

    26 12 2025

--- Todo App/todo.js
+++ Todo App/todo.js
@@ -1,7 +1,25 @@
 let todoList = [];
+
+displayItems();
 function addTodo() {
   let inputElement = document.querySelector("#todo-input");
   let todoItem = inputElement.value;
   todoList.push(todoItem);
   inputElement.value = "";
+  displayItems();
+}
+
+function displayItems() {
+  let containerElement = document.querySelector(".todo-container");
+
+  let newHtml = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    newHtml += `
+    <span>${todoList[i]}</span>
+    <button>Delete</button>
+   `;
+  }
+
+  containerElement.innerHTML = newHtml;
 }
