edf7dc7ba9ba414549670a9ef2a66f78071e87a7
shahinchowdory02 <shaheenchowdory80250@gmail.com>
2026-01-05 00:05:37 +0600

    04 01 2026

--- Todo App/todo.js
+++ Todo App/todo.js
@@ -1,37 +1,18 @@
-let todoList = [
-  { item: "Buy Milk", dueDate: "4/10/2023" },
-  { item: "Go to Collage", dueDate: "4/10/2023" },
-];
+let todoList = [];
 
 displayItems();
 function addTodo() {
-  let dateElement = document.querySelector("#todo-date");
-  let todoItem = inputElement.value;
-
-  let todoDate = dateElement.value;
-  todoList.push({ item: todoItem, dueDate: todoDate });
+  let todoElement = document.querySelector("todo-input");
+  let todoItem = todoElement.value;
+  todoList.push(todoItem);
   inputElement.value = "";
-  dateElement.value = "";
   displayItems();
 }
 
 function displayItems() {
-  let containerElement = document.querySelector(".todo-container");
-
-  let newHtml = "";
-
+  let displayElement = document.querySelector("#todo-items");
+  displayElement.innerText = "";
   for (let i = 0; i < todoList.length; i++) {
-    let item = todoList[i].item;
-    let dueDate = todoList[i].dueDate;
-
-    newHtml += `
-    <div>
-    <span>${item}</span>
-    <span>${dueDate}</span>
-    <button onclick="todoList.splice(${i}, 1); displayItems();">Delete</button>
-    </div>
-   `;
+    displayElement.innerText + "\n" + todoList[i];
   }
-
-  containerElement.innerHTML = newHtml;
 }
