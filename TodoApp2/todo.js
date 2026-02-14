bcfc1f712120908cf7f42cc5aa20b0cda37b7d55
shahinchowdory02 <shaheenchowdory80250@gmail.com>
2026-02-14 23:54:04 +0600

    14 02 2026

--- TodoApp2/todo.js
+++ TodoApp2/todo.js
@@ -1,26 +1,26 @@
-// let todoList = [];
-// displayItems();
-// function addTodo() {
-//   let inputElement = document.querySelector("#todo-input");
-//   let todoItem = todoElement.value;
-//   todoList.push(todoItem);
-//   todoElement.value = "";
-//   displayItems();
-// }
+let todoList = [];
+displayItems();
+function addTodo() {
+  let inputElement = document.querySelector("#todo-input");
+  let todoItem = inputElement.value;
+  todoList.push(todoItem);
+  inputElement.value = "";
+  displayItems();
+}
+
+function displayItems() {
+  let displayElement = document.querySelector(".todo-container");
+  displayElement.innerText = "";
+  for (let i = 0; i < todoList.length; i++) {
+    displayElement.innerText = displayElement.innerText + "\n" + todoList[i];
+  }
+}
 
 // function displayItems() {
+
 //   let displayElement = document.querySelector("#todo-input");
 //   displayElement.innerText = "";
 //   for (let i = 0; i < todoList.length; i++) {
 //     displayElement.innerText = displayElement.innerText + todoList[i] + "\n";
 //   }
 // }
-
-let todoList = [];
-
-displayItems();
-
-function addTodo() {
-  let inputElement = document.querySelector("#todo-input");
-  let todoItem = inputElement.value;
-}
